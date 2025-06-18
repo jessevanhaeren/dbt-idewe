cc19376edf12d7aa88f03859f501bbe307c408a9
jessevanhaeren <jesse@datashift.eu>
2025-06-18 17:49:33 +0200

    updated analyses

--- dbt_training/analyses/sem_dim_werkgever_werkgevers_per_regio.sql
+++ dbt_training/analyses/sem_dim_werkgever_werkgevers_per_regio.sql
@@ -2,6 +2,6 @@ SELECT
     [werkgever_regio],
     COUNT(DISTINCT [werkgevernummer]) AS aantal_werkgevers
 FROM 
-    ref('sem_dim_werkgever')
+    {{ref("sem_dim_werkgever")}}
 GROUP BY 
     [werkgever_regio]
